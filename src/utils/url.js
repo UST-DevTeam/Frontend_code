1c942beb67357a1c373696164973dbbe802ff83a
Vishal <vishal.yadav@fourbrick.com>
2024-04-25 11:59:54 +0530

    add buiild

--- src/utils/url.js
+++ src/utils/url.js
@@ -5,8 +5,8 @@
 // export const backendassetUrl="http://localhost:7980/"
 
 
-export const baseUrl="http://192.168.29.70:7980"
-export const backendassetUrl="http://192.168.29.70:7980/"
+// export const baseUrl="http://192.168.29.70:7980"
+// export const backendassetUrl="http://192.168.29.70:7980/"
 
 
 
@@ -20,8 +20,8 @@ export const backendassetUrl="http://192.168.29.70:7980/"
 
 
 
-// export const baseUrl="https://pmisfbapi.fourbrick.in"
-// export const backendassetUrl="https://pmisfbapi.fourbrick.in/"
+export const baseUrl="https://pmisfbapi.fourbrick.in"
+export const backendassetUrl="https://pmisfbapi.fourbrick.in/"
 
 
 
