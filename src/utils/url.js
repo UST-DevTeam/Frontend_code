8ea0a5bff9b19cbb5e3298e8f532b6b97647c857
Amit Gadia <amit@fourbrick.com>
2024-07-09 19:57:44 +0530

    09/07/2023/19:57

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,14 +1,14 @@
 // export const baseUrl="http://localhost:7980"
 // export const backendassetUrl="http://localhost:7980/"
 
-export const baseUrl="http://192.168.0.138:7980"
-export const backendassetUrl="http://192.168.0.138:7980/"
+// export const baseUrl="http://192.168.0.138:7980"
+// export const backendassetUrl="http://192.168.0.138:7980/"
 
 
 
 
-// export const baseUrl="https://pmisfbapi.fourbrick.in"
-// export const backendassetUrl="https://pmisfbapi.fourbrick.in/"   
+export const baseUrl="https://pmisfbapi.fourbrick.in"
+export const backendassetUrl="https://pmisfbapi.fourbrick.in/"   
 
 export const Urls={
 
