6274514bcba01828ae8a2a1cc57e0f1ad47fb06f
Shubham Pratap Singh <shubham.singh@fourbrick.com>
2024-07-09 19:50:01 +0530

    09-07-2024 : 19:49

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -100,7 +100,7 @@ const TopBar = ({ sidebarOpen, setsidebarOpenn }) => {
     };
 
     const handleLogoutClick = () => {
-        setPopupVisible(prev => !prev); // Toggle the popup visibility
+        setPopupVisible(prev => !prev);
     };
 
     const handleCancelClick = () => {
@@ -143,19 +143,20 @@ const TopBar = ({ sidebarOpen, setsidebarOpenn }) => {
                     </div>
                 </div>
                 {popupVisible && (
-                        <div ref={modalRef} className="absolute right-4 mt-10 w-1/6  bg-slate-200 shadow-lg rounded-md z-50 border-[1.2px] border-black">
+                        <div ref={modalRef} className="absolute right-1 mt-10 w-[150px]  bg-[#4f5760] shadow-lg rounded-md z-[100000] border-[1.5px] border-[#13b497]">
                             <div className="p-2">
-                                <div className=" text-center">
-                                    <p className="text-gray-900 font-medium">{roleName}</p>
+                                <div className="flex space-x-1 ml-2">
+                                    <Unicons.UilUser fill="#13b497" className="hover:text-heading" />
+                                    <p className="text-white font-medium font-">{roleName}</p>
                                 </div>
                                 {/* <div className="">
                                     <button className="w-full text-center text-gray-700 hover:text-blue-600 py-2 font-medium">Change Password</button>
                                 </div> */}
-                                <div className="flex justify-center">
+                                <div className="flex space-x-1 ml-2 mt-3">
                                     <button>
-                                    <div onClick={calllogout} className='dark:text-white flex space-x-1 cursor-pointer hover:border-solid hover:border-[#d5d7df] hover:border-[1.2px] hover:p-1 hover:rounded-md'>
-                                    <span className='text-black pr-1 hover:cursor-pointer font-medium'>Logout</span>
+                                    <div onClick={calllogout} className='dark:text-white flex space-x-1 cursor-pointer'>
                                     <Unicons.UilSignout fill="#13b497" className="hover:text-heading hover:cursor-pointer" />
+                                    <span className='text-white pr-1 hover:cursor-pointer font-medium'>Logout</span>
                                     </div>
                                     </button>
                                 </div>
