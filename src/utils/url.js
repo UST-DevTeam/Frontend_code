05de1af5b30822984bdd67bb18d6157b21ed0f8f
Vishal Yadav <vishal.yadav@fourbrick.com>
2024-07-29 22:46:01 +0530

    29/07/2024/10:45

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,5 +1,5 @@
-export const baseUrl="http://localhost:7980"
-export const backendassetUrl="http://localhost:7980/"
+// export const baseUrl="http://localhost:7980"
+// export const backendassetUrl="http://localhost:7980/"
 
 // export const baseUrl="http://192.168.0.118:7990"
 // export const backendassetUrl="http://192.168.0.118:7990/"
@@ -10,8 +10,8 @@ export const backendassetUrl="http://localhost:7980/"
 // export const baseUrl="https://pmisfbapi.fourbrick.in"
 // export const backendassetUrl="https://pmisfbapi.fourbrick.in/"   
 
-// export const baseUrl="https://pmisapi.mcpsinc.com"
-// export const backendassetUrl="https://pmisapi.mcpsinc.com/"   
+export const baseUrl="https://pmisapi.mcpsinc.com"
+export const backendassetUrl="https://pmisapi.mcpsinc.com/"   
 
 export const Urls={
 
