a5fa535783929232d0ddf274ba1834be2a2a1dc2
Shubham Pratap Singh <shubham.singh@fourbrick.com>
2024-08-12 19:19:17 +0530

    12-08-2024 : 19:18

--- src/components/NewMultiSelect.jsx
+++ src/components/NewMultiSelect.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import { MultiSelect } from "react-multi-select-component";
 
-const NewMultiSelects = ({ label, option = [], value = [], required = false, placeholder = "Select...", cb = (data) => { }, ...props }) => {
+const NewMultiSelects = ({ label, option = [], value = [], required = false, placeholder = "Select...", cb = (data) => { }, bg='bg-[#3e454d]', ...props }) => {
     const [length, setLength] = useState(0)
     return (
         <div className={`max-w-[170px] min-w-[100px]  relative p-0  ${props?.height || ''} ${props?.className || ''} w-full`}>
             {/* <label htmlFor={label} className='text-white ml-2'>{required ? <span className='text-red-600 mr-1 '>*</span> : <></>}{label + (length > 0 ? " ( " + length + " )" : "")}</label> */}
             {/* <label htmlFor={label} className='text-white ml-2'>{required ? <span className='text-red-600 mr-1 '>*</span> : <></>}{label}</label> */}
             <MultiSelect
-                className="outline-none font-semibold rounded-md border-main mt-[4px] border-solid border-[#64676d] border-[1.2px]"
+                className={`outline-none font-semibold rounded-md border-main mt-[4px] border-solid border-[#64676d] border-[1.2px] ${bg}`}
                 options={option}
                 value={value}
                 placeholder={placeholder}
