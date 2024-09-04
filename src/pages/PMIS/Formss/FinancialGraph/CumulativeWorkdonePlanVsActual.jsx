37ec484aeadc89789b25de714c99a96770527f34
Shubham Pratap Singh <shubham.singh@fourbrick.com>
2024-09-04 13:19:58 +0530

    ytresdncvvjnkkvvv13:19

--- src/pages/PMIS/Formss/FinancialGraph/CumulativeWorkdonePlanVsActual.jsx
+++ src/pages/PMIS/Formss/FinancialGraph/CumulativeWorkdonePlanVsActual.jsx
@@ -376,7 +376,7 @@ const CumulativeWorkdonePlanVsActual = () => {
   return (
     <div className="bg-transparent border-[1.5px] border-pcol rounded-md h-full p-4">
          <div className="text-center mb-4">
-            <h1 className="text-white text-base font-bold">Cumulative - Plan VS Actual Workdone</h1>
+            <h1 className="text-[#f4d3a8] font-bold text-lg whitespace-nowrap underline">Cumulative - Plan VS Actual Workdone</h1>
         </div>
         <div className="flex items-center justify-between space-x-10">
         <div className="flex space-x-2 items-center w-full">
@@ -424,12 +424,12 @@ const CumulativeWorkdonePlanVsActual = () => {
             <Button
               classes="w-12 h-10 text-white mt-1 flex justify-center bg-transparent border-solid border-[#64676d] border-2"
               onClick={handleFilter}
-              icon={<UilSearch size="18" className={"hello"} />}
+              icon={<UilSearch size="36" className="text-[#f4d3a8]"  />}
             ></Button>
             <Button
               classes="w-12 h-10 text-white mt-1 flex justify-center bg-transparent border-solid border-[#64676d] border-2"
               onClick={handleClear}
-              icon={<UilRefresh size="36" />}
+              icon={<UilRefresh size="36" className = "text-[#f4d3a8]"/>}
             ></Button>
           </div>
         </div>
