27b6de92099d90e0d444dcbd82a2e9055ab0d9be
ThakarMandyal <shubhambarwalp7258@gmail.com>
2025-06-26 16:56:53 +0530

    shubham fgjfgg

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,5 +1,5 @@
-export const baseUrl = "https://devapi.mcpspmis.com"
-export const backendassetUrl = "https://devapi.mcpspmis.com/"
+// export const baseUrl = "https://devapi.mcpspmis.com"
+// export const backendassetUrl = "https://devapi.mcpspmis.com/"
 
 // export const baseUrl = "https://api.mcpspmis.com"
 // export const backendassetUrl = "https://api.mcpspmis.com/"
@@ -7,8 +7,8 @@ export const backendassetUrl = "https://devapi.mcpspmis.com/"
 // export const backendassetUrl="https://androidapi.mcpspmis.com/"
 // export const baseUrl = "http://192.168.1.9:7980";
 // export const backendassetUrl = "http://192.168.1.9:7980";
-// export const baseUrl = "http://192.168.1.6:7980";
-// export const backendassetUrl = "http://192.168.1.6:7980/";
+export const baseUrl = "http://192.168.1.6:7980";
+export const backendassetUrl = "http://192.168.1.6:7980/";
 
 export const Urls = {
   login: "/login",
