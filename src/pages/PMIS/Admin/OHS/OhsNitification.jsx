8ff227438c02592e526315b49ee9579a9dfd656f
Amit Gadia <amit@fourbrick.com>
2025-07-10 13:44:35 +0530

    shdfks dhf

--- src/pages/PMIS/Admin/OHS/OhsNitification.jsx
+++ src/pages/PMIS/Admin/OHS/OhsNitification.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import AdvancedTable from '../../../../components/AdvancedTable';
+import Button from '../../../../components/Button';
 
 const OhsNitification = () => {
+  const table = {
+    columns: [
+      {
+        name: "Milestone",
+        value: "Milestone",
+        style: "text-center min-w-[100px]",
+      },
+    ],
+    properties: {
+      rpp: [10, 20, 50, 100],
+    },
+    filter: [],
+  };
   return (
     <div className=''>
-      
+      <AdvancedTable
+        headerButton={
+          <div className="flex gap-2">
+            <Button
+              name={"Add New"}
+              classes="w-auto"
+              onClick={()=>{}}
+            />
+          </div>
+        }
+        table={table}
+        
+        tableName="OHS Notifications"
+        TableHeight="h-[68vh]"
+        
+        data={[]}
+        
+        totalCount={0}
+        heading="Total Count :-"
+     
+      />
     </div>
   )
 }
